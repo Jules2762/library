2bedaed01d443b4e6f0988401513b677a8a9deae
Jules2762 <jules.haisoa@gmail.com>
2024-01-13 16:02:39 +0100

    add content accueil

---
+++ src/components/client/carousel.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div class="w-screen flex justify-center items-center">
+        <div class="max-w-[93vw] flex justify-between items-center">
+            <div class="w-4/12">
+                Bienvenu chez nous
+            </div >
+            <div class="w-8/12"><img src="../../assets/alexander.jpg" alt=""></div>
+        </div>
+    </div>
+</template>
+<script>
+import Transition  from 'vue';
+export default {
+    components:{
+        
+    }
+}
+</script>
+<style scoped>
+    .v-enter-active {
+        transition:transform  0.5s;
+    }
+    .v-enter-from{
+        transform: translateX(200px);
+    }
+</style>
